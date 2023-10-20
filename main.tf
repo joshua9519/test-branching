77b0452a193477da222e4e56766dcbe76a118726
Josh Hill <josh@vetspire.com>
2023-10-20 16:01:43 +0100

    Add bucket iam member

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "google_storage_bucket" "default" {
     location = "us-central1"
     name = var.value
 }
+
+resource "google_storage_bucket_iam_member" "name" {
+    bucket = google_storage_bucket.default.name
+    member = "me"
+    role = "roles/storage.admin"
+}
