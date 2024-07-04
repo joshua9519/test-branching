1d203cdab6093abf9ccb0dc8726c3d050c3d71f7
Josh Hill <josh@vetspire.com>
2024-07-04 13:50:47 +0100

    Format

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "google_storage_bucket" "default" {
-    location = "us-east1"
-    name = var.value
-    project = var.project
+  location = "us-east1"
+  name     = var.value
+  project  = var.project
 }
 
 resource "google_storage_bucket_iam_member" "name" {
-    bucket = google_storage_bucket.default.name
-    member = data.google_client_openid_userinfo.me.email
-    role = "roles/storage.admin"
+  bucket = google_storage_bucket.default.name
+  member = data.google_client_openid_userinfo.me.email
+  role   = "roles/storage.admin"
 }
 
 data "google_client_openid_userinfo" "me" {
